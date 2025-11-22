ec12f339da39395f8caed861cde75373a58d6f0b
sathish.thangaraj <sathish.thangaraj.2022@gmail.com>
2025-11-22 15:16:10 +0800

    Initial commit

---
+++ src/main/java/com/example/spring/boot/demo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World! 👋. This sample project is to check the CI CD setup in jenkins.";
+    }
+}
---
+++ src/test/java/com/example/spring/boot/demo/SpringBootK8ExampleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8ExampleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
